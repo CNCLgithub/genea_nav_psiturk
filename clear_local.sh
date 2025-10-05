7d58610f40b7c1e3e650b120ccb985b97aec4480
jakiroshah <aalap.shah@yale.edu>
2025-10-05 16:35:26 -0400

    added intructions, final v1.0

---
+++ clear_local.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf psiturk/.psiturk_history
+rm -rf psiturk/participants.db
+rm -rf psiturk/server.log
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-CONT_NAME="psiturk.sif"
-
-singularity exec "$CONT_NAME" bash -c "cd psiturk && psiturk"
