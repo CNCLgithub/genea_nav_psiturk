8aa9ee79c15030d492d7a39a43ca4c935a197105
jakiroshah <aalap.shah@yale.edu>
2025-10-05 16:33:38 -0400

    added intructions, final v1.0

--- setup.sh
+++ setup.sh
@@ -34,6 +34,13 @@ fi
 # download stimulus set
 if [[ "$@" =~ "data" ]] || [[ "$@" =~ "all" ]];then
     echo "Pulling data..."
+    wget "https://yaleedu-my.sharepoint.com/:u:/g/personal/aalap_shah_yale_edu/EdWUyHlrPk9PgVellFs_bBwBc1OlYzcdGyffu8r6yGrr7Q?e=dEOt4V&download=1" -O "images.zip"
+    chmod +777 images.zip
+    unzip images.zip
+    mv images/* psiturk/static/images/
+    rm -rf images
+    rm -rf images.zip
+
     wget "https://yaleedu-my.sharepoint.com/:u:/g/personal/aalap_shah_yale_edu/EXNoP4NxxsBKvtbfYteBH2MBeAoE5UyOQUP1J5Hn1Un-hg?e=wiV5OL&download=1" -O "stimuli_videos.zip"
     chmod +777 stimuli_videos.zip
     unzip stimuli_videos.zip
